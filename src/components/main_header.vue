8847f00172185e0a6daa1f6f90c724984823c68a
sanji121 <sanjeeb020@gmail.com>
2020-02-11 04:01:20 +0000

    w

---
+++ src/components/main_header.vue
@@ -0,0 +1,28 @@
+<template>
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-inverse">
+  <div class="container-fluid">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item mr-2">
+          <router-link :to="{name:'register'}" class="btn btn-secondary">
+              <span class="glyphicon glyphicon-user"></span> Sign Up
+            </router-link>
+        </li>
+        <li class="nav-item">
+            <router-link :to="{name:'login'}" class="btn btn-info">
+              <span class="glyphicon glyphicon-log-in"></span> Login
+            </router-link>
+        </li>
+      </ul>
+  </div>
+</nav>
+
+</template>
+
+<script>
+
+export default{
+ name:'main_header'
+
+}
+</script>
